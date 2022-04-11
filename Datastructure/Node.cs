5938309d231f4deb4c8c33bd6401569d8a059ff9
Rajesh Yandigeri <rajeshyandigeri007@gmail.com>
2022-04-11 23:08:40 +0530

    First time

---
+++ Datastructure/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure
+{
+    internal class Node
+    {
+        private string data;
+        private Node next = null;
+
+        public string data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
